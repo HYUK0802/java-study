cbff598b520a77ad558d3d155d1f0624d9fd7f17
hyuk <tig05205@naver.com>
2023-12-17 18:14:32 +0900

    java day1

---
+++ src/variable/Var1.java
@@ -0,0 +1,12 @@
+package variable;
+
+import java.util.Scanner;
+
+public class Var1 {
+    public static void main(String[] args) {
+
+        System.out.println(10);
+        System.out.println(10);
+        System.out.println(10);
+    }
+}
---
+++ src/variable/Var6.java
@@ -0,0 +1,14 @@
+package variable;
+
+import java.util.Random;
+
+public class Var6 {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int a;
+        a = rand.nextInt(99);
+
+        System.out.println(a);
+
+    }
+}
