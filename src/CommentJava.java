404e75f18b77ee108c16900a246aabd1b3cd8899
hyuk <tig05205@naver.com>
2024-01-26 01:33:08 +0900

    java

--- src/CommentJava.java
+++ src/CommentJava.java
@@ -1,6 +1,10 @@
 public class CommentJava {
     public static void main(String[] args) {
         System.out.println("hello java1"); // hello java1을 출력합니다.
+        int a = 7;
+        int b = 8;
+        int c = a % b;
+        System.out.println(c);
 //        System.out.println("hello java2");
 
         /*
